a9b08717a04c705810fae059f556e743a4bd9b20
Marcos Tanaka <marcosatanaka@gmail.com>
2016-03-20 14:31:03 -0300

    Initial commit

---
+++ src/main/java/br/com/unicesumar/EscolaDeTiGrupo6Application.java
@@ -0,0 +1,12 @@
+package br.com.unicesumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaDeTiGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaDeTiGrupo6Application.class, args);
+	}
+}
